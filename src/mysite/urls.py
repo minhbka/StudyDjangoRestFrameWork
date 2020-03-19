f9ce6e14c71a6f6fb4610202283c8c0a1e2aaf4a
minhphanvan <minhpv@kormedi.com>
2020-03-19 22:42:31 +0900

    Updating User Accounts with Forms

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -20,12 +20,13 @@ from personal.views import(
 )
 
 from account.views import (
-    registration_view, logout_view, login_view
+    registration_view, logout_view, login_view, account_view
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('account/', account_view, name='account')
 ]
