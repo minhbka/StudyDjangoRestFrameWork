4c1f7ccd333dabb0ccfe18e6432f311e05d85b01
Phan Minh <minhpv@kormedi.com>
2020-03-19 17:07:28 +0900

    Customizing Django Admin (search and filtering)

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render
-from personal.models import Question
+from account.models import Account
 # Create your views here.
 
 def home_screen_view(request):
     context = {}
-    # context['some_string'] =  "this is some string that I'm passing to the view"
-    # context['some_number'] = 12345
 
-    # list_of_value = []
-    # list_of_value.append("first entry")
-    # list_of_value.append("second entry")
-    # list_of_value.append("third entry")
-    # list_of_value.append("fourth entry")
+    accounts = Account.objects.all()
 
-    # context['list_of_value'] = list_of_value
-
-    questions = Question.objects.all()
-    context['questions'] = questions
+    context['accounts'] = accounts
     
     return render(request, "personal/home.html", context)
