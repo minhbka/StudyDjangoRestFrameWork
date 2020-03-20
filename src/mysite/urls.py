04995218328aeb60ccc9c9230e0623fcd7b1d6d6
Phan Minh <minhpv@kormedi.com>
2020-03-20 16:41:06 +0900

    Modeling the Blog using Reddit.com

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,6 +29,7 @@ from account.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
+    path('blog/', include('blog.urls', 'blog')),
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
