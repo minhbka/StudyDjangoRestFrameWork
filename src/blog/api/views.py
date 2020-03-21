8773450540e80bfdec7371cad48328a71a348a74
Phan Minh <minhpv@kormedi.com>
2020-03-21 19:25:19 +0900

    GET, UPDATE, POST, DELETE - Function Views (Django REST framework)

--- src/blog/api/views.py
+++ src/blog/api/views.py
@@ -17,3 +17,54 @@ def api_detail_blog_view(request, slug):
     if request.method == 'GET':
         serializer = BlogPostSerialize(blog_post)
         return Response(serializer.data)
+
+
+@api_view(['PUT',])
+def api_update_blog_view(request, slug):
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = BlogPostSerialize(blog_post, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["success"] = "update successful"
+            return Response(data=data)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', ])
+def api_delete_blog_view(request, slug):
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        operation = blog_post.delete()
+        data = {}
+        if operation:
+            data["success"] = "delete successful"
+        else:
+            data["falure"] = "delete failed"
+
+        return Response(data=data)
+
+
+@api_view(['POST',])
+def api_create_blog_view(request):
+    account = Account.objects.get(pk=1)
+    blog_post = BlogPost(author=account)
+
+    if request.method == "POST":
+        serializer = BlogPostSerialize(blog_post, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
